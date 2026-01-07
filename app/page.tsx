689b09d5e47da63d9336439647fd282e81910545
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-08 00:59:55 +0330

    NIK_TASK

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeaderComponent from "@/components/modules/Header";
 import HeroComponent from "@/components/modules/Hero";
+import TopSlider from "@/components/modules/TopSlider";
 
 const HomePage = () => {
   return (
     <div className="container border mx-auto py-5 border-sky-900 min-h-screen">
       <HeaderComponent />
       <HeroComponent />
+      <TopSlider />
     </div>
   );
 };
