7676e35871f9a931c539b618661b89cb93885c9a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-08 00:58:18 +0330

    NIK_TASK

--- components/modules/Hero/index.tsx
+++ components/modules/Hero/index.tsx
@@ -2,25 +2,25 @@ import Image from "next/image";
 
 const HeroComponent = () => {
   return (
-    <div dir="rtl" className="w-full border flex flex-col items-center justify-center h-100">
-      <div className="flex items-center gap-3">
-        <span className="text-[40px] font-bold text-nik-foreground">بزار کُدت</span>
+    <div dir="rtl" className="w-full border flex flex-col items-center justify-center h-80 lg:h-100">
+      <div className="flex items-center gap-2 lg:gap-3">
+        <span className="text-[22px] lg:text-[40px] font-extrabold lg:font-bold text-nik-foreground">بزار کُدت</span>
         <Image
           src={"/images/header/header_logo.png"}
-          className="h-20 w-14 -translate-y-3"
+          className="h-14 lg:h-20 w-10 lg:w-14 -translate-y-3 lg:-translate-y-3"
           width={500}
           height={500}
           alt="header-logo"
         />
-        <span className="text-[40px] font-bold text-nik-foreground">حرف بزنه ...</span>
+        <span className="text-[22px] lg:text-[40px] font-extrabold lg:font-bold text-nik-foreground">حرف بزنه...</span>
       </div>
       <div>
-        <h3 className="text-[36px] font-medium text-nik-foreground">صفحه ای ساده اما به جزئیات ریز!</h3>
+        <h3 className="text-[18px] lg:text-[36px] font-medium text-nik-foreground">صفحه ای ساده اما به جزئیات ریز!</h3>
       </div>
       <div>
         <Image
           src={"/images/header/header_arrow.png"}
-          className="h-26 w-16 translate-y-3"
+          className="w-8 lg:w-16 h-18 lg:h-26  translate-y-3"
           width={500}
           height={500}
           alt="header-logo"
