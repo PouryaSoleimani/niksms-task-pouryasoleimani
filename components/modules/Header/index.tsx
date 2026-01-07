a411e33ada172fdc70eafb18d6993c32954e682f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-07 21:17:08 +0330

    NIK_TASK

--- components/modules/Header/index.tsx
+++ components/modules/Header/index.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { Circle, Menu, X } from "lucide-react";
+import { Circle } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { AnimatePresence, motion } from "motion/react";
 import { cn } from "@/lib/utils";
 
 const HeaderComponent = () => {
@@ -15,47 +13,6 @@ const HeaderComponent = () => {
 
   return (
     <div className="flex items-center-safe justify-between px-4 relative left-0 top-0">
-      {/* <Button
-        onClick={() => setIsExpanded(!isExpanded)}
-        size={"auto"}
-        variant={isExpanded ? "focused" : "default"}
-        className="rounded-full  p-2 px-3.5 lg:p-2 lg:px-6 flex items-center-safe justify-center font-bold gap-1 tracking-wide"
-      >
-        <p className="hidden lg:block">منو تسک ها</p>
-        <label htmlFor="burger" className={cn("burger", isExpanded ? "*:bg-[#dffca1]" : "*:bg-[#094020]")}>
-          <input type="checkbox" id="burger" checked={isExpanded} />
-          <span className={cn(isExpanded ? "focused" : "")}></span>
-          <span className={cn(isExpanded ? "focused" : "")}></span>
-          <span className={cn(isExpanded ? "focused" : "")}></span>
-        </label>
-      </Button>
-      <AnimatePresence initial={false}>
-        {isExpanded && (
-          <motion.ul
-            className="border absolute top-14 left-8"
-            initial={{ opacity: 0, y: 0 }}
-            animate={{
-              opacity: 1,
-              y: 1,
-              transition: { when: "beforeChildren", delayChildren: 0.3, staggerChildren: 0.7 },
-            }}
-            exit={{ opacity: 0, y: 0 }}
-            key="box"
-          >
-            {menuItems.map((item) => (
-              <Link
-                key={item.id}
-                href={item.href}
-                className="bg-nik-primary flex items-center justify-end gap-2 px-4 py-2 rounded-full text-md font-semibold text-sm tracking-tight cursor-pointer hover:bg-(--nik-foreground) my-2"
-              >
-                {item.title}
-                <Circle className="size-4" />
-              </Link>
-            ))}
-          </motion.ul>
-        )}
-      </AnimatePresence> */}
-
       <label
         className={cn(
           "main border border-transparent hover:border-[#094020]",
@@ -64,7 +21,7 @@ const HeaderComponent = () => {
       >
         <p className="hidden lg:block text-sm">منو تسک ها</p>
         <input className="inp" type="checkbox" checked={!isExpanded} onChange={() => setIsExpanded(!isExpanded)} />
-        <div className={cn("bar", isExpanded && "expanded")}>
+        <div className={cn("bar", isExpanded && "expanded max-[820px]:-translate-x-1")}>
           <span className={cn("top bar-list", isExpanded && "bg-[#dffca1]!")}></span>
           <span className={cn("middle bar-list", isExpanded && "bg-[#dffca1]!")}></span>
           <span className={cn("bottom bar-list", isExpanded && "bg-[#dffca1]!")}></span>
@@ -77,13 +34,6 @@ const HeaderComponent = () => {
             </Link>
           ))}
         </section>
-
-        {/* <section className="menu-container">
-          <div className="menu-list">اسلایدر</div>
-          <div className="menu-list">تب منو</div>
-          <div className="menu-list">فرم ثبت نام</div>
-          <div className="menu-list">رنج اسلایدر</div>
-        </section> */}
       </label>
 
       <p className="font-semibold text-nik-foreground text-xl"> پوریا سلیمانی</p>
