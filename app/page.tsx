442afa086fe2163bc579685f8dada11e3688ed8e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-08 14:21:43 +0330

    NIK__TASK

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+import GapComponent from "@/components/modules/Gap";
 import HeaderComponent from "@/components/modules/Header";
 import HeroComponent from "@/components/modules/Hero";
 import TopSlider from "@/components/modules/TopSlider";
 
 const HomePage = () => {
   return (
-    <div className="w-screen m-0 lg:container border lg:mx-auto py-5 border-sky-900 min-h-screen">
+    <div className="w-screen m-0 lg:container 2xl:max-w-7xl! border lg:mx-auto py-5 border-sky-900 min-h-screen">
       <HeaderComponent />
       <HeroComponent />
       <TopSlider />
+      <GapComponent />
     </div>
   );
 };
---
+++ components/modules/Gap/index.tsx
@@ -0,0 +1,5 @@
+const GapComponent = () => {
+  return <div className="border-[3px] border-dashed h-20 w-[88%] mx-auto border-[#f2f2f0] border-t-0 border-b-0"></div>;
+};
+
+export default GapComponent;
