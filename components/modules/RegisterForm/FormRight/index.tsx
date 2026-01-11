f821db5afe7b7a7fbb958aa5673ca6f483a6165d
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-11 15:49:19 +0330

    NIK__TASK

--- components/modules/RegisterForm/FormRight/index.tsx
+++ components/modules/RegisterForm/FormRight/index.tsx
@@ -7,7 +7,8 @@ import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import ErrorField from "../../ErrorField";
-import { CheckCircle2Icon, InfoIcon, XCircleIcon } from "lucide-react";
+import { CheckCircle2Icon, Eye, EyeClosed, InfoIcon, XCircleIcon } from "lucide-react";
+import { useState } from "react";
 
 const FormSchema = z.object({
   username: z.string("وارد کردن نام الزامی است").min(1, "وارد کردن نام الزامی می باشد"),
@@ -16,11 +17,13 @@ const FormSchema = z.object({
     .string("وارد کردن پسورد الزامی است")
     .min(8, "رمز عبور باید حداقل 8 حرف باشد")
     .includes("!@#$%^&*()_+-=", "رمز عبور باید شامل کاراکتر های خاص مثل #$! باشد"),
+  // .regex(/^\d+$/, "رمز عبور باید شامل اعداد باشد"),
 });
 
 export type FormTypes = z.infer<typeof FormSchema>;
 
 const FormRightComponent = () => {
+  const [inputType, setInputType] = useState<"password" | "text">("password");
   //^ REACT__HOOK__FORM
   const methods = useForm<FormTypes>({
     mode: "onChange",
@@ -41,7 +44,15 @@ const FormRightComponent = () => {
     methods.reset();
   }
 
-  console.info("ERRORS => ", methods.formState.errors.password?.type);
+  function toggleInputTypeHandler() {
+    if (inputType === "password") {
+      setInputType("text");
+    } else {
+      setInputType("password");
+    }
+  }
+
+  console.info("ERRORS => ", Array(methods.formState.errors.password));
   return (
     <div className="min-w-full">
       <FormProvider {...methods}>
@@ -78,13 +89,32 @@ const FormRightComponent = () => {
                 <Label className="mb-0.5">رمز عبور</Label>
                 <Input
                   autoComplete="false"
-                  type="password"
+                  type={inputType}
                   value={field.value}
                   onChange={field.onChange}
                   aria-invalid={!!methods.formState.errors.password}
                   placeholder={"رمز عبور"}
-                  className={cn(methods.getValues(field.name).length > 0 && !methods.formState.errors.password && "bg-[#f2f2f0]")}
+                  className={cn("relative inset-0", methods.getValues(field.name).length > 0 && !methods.formState.errors.password && "bg-[#f2f2f0]")}
                 />
+                {inputType === "password" ? (
+                  <Button
+                    type="button"
+                    onClick={toggleInputTypeHandler}
+                    variant={"ghost"}
+                    className="w-fit absolute left-1 top-[42.5%] !px-1 hover:bg-transparent group"
+                  >
+                    <Eye className="size-5 text-stone-600 group-hover:text-stone-800 transition-all duration-250" />
+                  </Button>
+                ) : (
+                  <Button
+                    type="button"
+                    onClick={toggleInputTypeHandler}
+                    variant={"ghost"}
+                    className="w-fit absolute left-1 top-[42.5%] !px-1 hover:bg-transparent group"
+                  >
+                    <EyeClosed className="size-5 text-stone-600 group-hover:text-stone-800 transition-all duration-250" />
+                  </Button>
+                )}
                 <button
                   type="button"
                   className={cn("absolute cursor-pointer group right-1.5 top-1/2", methods.formState.errors.password && "top-7")}
@@ -120,7 +150,7 @@ const FormRightComponent = () => {
 
           {/* PASSWORD HINTS */}
           <div className="flex flex-col gap-2 text-xs text-stone-500">
-            <p className="flex items-center gap-1">
+            <p id="PASSWORD__MIN__LENGTH" className="flex items-center gap-1">
               {!methods.formState.dirtyFields.password ? (
                 <div className={cn("flex items-center gap-1 text-stone-500")}>
                   <InfoIcon className="size-4 translate-y-[1px]" />
@@ -140,23 +170,40 @@ const FormRightComponent = () => {
                 )
               )}
             </p>
-            <p className="flex items-center gap-1">
-              <CheckCircle2Icon className="size-4" />
-              رمز عبور باید شامل اعداد باشد .
+            <p id="PASSWORD__CONTAINS__NUMBERS" className="flex items-center gap-1">
+              {!methods.formState.dirtyFields.password ? (
+                <div className={cn("flex items-center gap-1 text-stone-500")}>
+                  <InfoIcon className="size-4 translate-y-[1px]" />
+                  رمز عبور باید شامل اعداد باشد
+                </div>
+              ) : methods.formState.dirtyFields.password && Array(methods.formState.errors.password).some((item) => item?.type === "too_small") ? (
+                <div className="flex items-center gap-1 text-destructive">
+                  <XCircleIcon className="size-4 translate-y-[1px]" />
+                  رمز عبور باید شامل اعداد باشد
+                </div>
+              ) : (
+                Array(methods.formState.errors.password).length && (
+                  <div className={cn("flex items-center gap-1 text-nik-foreground")}>
+                    <CheckCircle2Icon className="size-4 translate-y-[1px]" />
+                    رمز عبور باید شامل اعداد باشد
+                  </div>
+                )
+              )}
             </p>
-            <p className="flex items-center gap-1">
+            <p id="PASSWORD__CONTAINS__SYMBOLS" className="flex items-center gap-1">
               {!methods.formState.dirtyFields.password ? (
                 <div className={cn("flex items-center gap-1 text-stone-500")}>
                   <InfoIcon className="size-4 translate-y-[1px]" />
                   رمز عبور باید شامل کاراکتر های خاص مثل !@#% باشد
                 </div>
-              ) : methods.formState.dirtyFields.password && methods.formState.errors.password?.type === "invalid_format" ? (
+              ) : methods.formState.dirtyFields.password &&
+                Array(methods.formState.errors.password).some((item) => item?.type === "invalid_format") ? (
                 <div className="flex items-center gap-1 text-destructive">
                   <XCircleIcon className="size-4 translate-y-[1px]" />
                   رمز عبور باید شامل کاراکتر های خاص مثل !@#% باشد
                 </div>
               ) : (
-                Array(methods.formState.errors.password).length && (
+                Array(methods.formState.errors.password).every((item) => item?.type !== "invalid_format") && (
                   <div className={cn("flex items-center gap-1 text-nik-foreground")}>
                     <CheckCircle2Icon className="size-4 translate-y-[1px]" />
                     رمز عبور باید شامل کاراکتر های خاص مثل !@#% باشد
