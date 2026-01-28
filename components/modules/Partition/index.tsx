fa7200a8b6de7eead541385a61f9bec65fb96362
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-29 00:40:40 +0330

    [RESPONSIVE] TABS___SECTION

--- components/modules/Partition/index.tsx
+++ components/modules/Partition/index.tsx
@@ -5,17 +5,23 @@ interface PartitionProps {
   faTitle: string;
   children: React.ReactNode;
   id?: string;
-  classNames?: { wrapper?: string, bottom?: string, top?: string }
+  classNames?: { wrapper?: string; bottom?: string; top?: string };
 }
 
 const PartitionComponent = ({ enTitle, children, faTitle, id, classNames }: PartitionProps) => {
   return (
-    <div id={id} className={cn("w-full mx-auto bg-[#f2f2f0]  lg:rounded-[30px] p-3.5 lg:p-6 flex items-center justify-center", classNames?.wrapper)}>
+    <div
+      id={id}
+      className={cn("w-full mx-auto bg-[#f2f2f0]  lg:rounded-[34px] p-3.5 lg:p-[24px] flex items-center justify-center", classNames?.wrapper)}
+    >
       <div className="bg-[#f2f2f0] overflow-hidden rounded-lg flex flex-col w-full justify-start h-full gap-2.5 lg:gap-[14px]">
         {/* TOP SECTION */}
         <div
           id="TOP"
-          className={cn("w-full font-bold text-md lg:text-xl text-nik-foreground px-3 py-2 lg:p-[15px] rounded-lg lg:rounded-[16px] bg-white flex items-center-safe justify-between", classNames?.top)}
+          className={cn(
+            "w-full font-bold text-md lg:text-xl text-nik-foreground px-3 py-2 lg:p-[15px] rounded-lg lg:rounded-[18px] bg-white flex items-center-safe justify-between",
+            classNames?.top,
+          )}
         >
           <span>{enTitle}</span>
           <span className="flex gap-2 items-center">
@@ -25,7 +31,10 @@ const PartitionComponent = ({ enTitle, children, faTitle, id, classNames }: Part
         </div>
 
         {/* BOTTOM SECTION */}
-        <div id="BOTTOM__WRAPPER" className={cn("flex flex-col lg:flex-row items-center-safe gap-4 rounded-lg bg-white lg:rounded-[16px]", classNames?.bottom)}>
+        <div
+          id="BOTTOM__WRAPPER"
+          className={cn("flex flex-col lg:flex-row items-center-safe gap-4 rounded-lg bg-white lg:rounded-[22px]", classNames?.bottom)}
+        >
           {children}
         </div>
       </div>
--- components/modules/Tabs/index.tsx
+++ components/modules/Tabs/index.tsx
@@ -8,19 +8,20 @@ import { SheetTitle } from "@/components/animate-ui/primitives/radix/sheet";
 import PartitionComponent from "../Partition";
 import useIsMobileXS from "@/hooks/useIsMobileXS";
 import useIsTablet from "@/hooks/useIsTablet";
+import useIsMobile from "@/hooks/useIsMobile";
 
 const TabsComponent = () => {
   const isXS = useIsMobileXS();
   const isTablet = useIsTablet();
-
+  const isMobile = useIsMobile();
   return (
     <PartitionComponent
       enTitle="TabMenu"
       faTitle="تب منو"
       id="TAB__MENU"
-      classNames={{ wrapper: "h-[634px] sm:h-[750px] lg:h-[627px]", top: "h-[45px] lg:h-[61px]" }}
+      classNames={{ wrapper: "h-[634px] sm:h-[750px] lg:h-[627px]", top: "h-[45px] lg:h-[61px]", bottom: "xs:pb-0" }}
     >
-      <Tabs dir="rtl" defaultValue="1" className="overflow-hidden w-full h-full p-2.5">
+      <Tabs dir="rtl" defaultValue="1" className="overflow-hidden w-full h-full p-2.5 pb-0">
         <TabsList className="TAB__LIST flex w-full lg:w-[92%] lg:mx-auto lg:mt-8 overflow-x-auto lg:overflow-x-hidden overflow-y-hidden whitespace-nowrap gap-1 shrink-0! bg-nik-primary border border-[#D5f39B] rounded-lg lg:rounded-2xl scrollbar-hide touch-pan-x scroll-smooth snap-x snap-mandatory">
           {tabList.map((item: TabItem) => (
             <TabsTrigger
@@ -37,19 +38,19 @@ const TabsComponent = () => {
         {tabList.map((item: TabItem) => (
           <TabsContent key={item.id} value={item.id.toString()} className="mt-4 lg:px-16 flex flex-col lg:flex-row gap-3 overflow-hidden">
             {/* TOP SECTION */}
-            <div className="xs:p-3 md:p-8 h-1/2 lg:flex lg:flex-col lg:justify-start lg:mt-10 lg:gap-5 lg:basis-1/2">
-              <div className="flex items-center-safe font-bold gap-3 text-lg">
+            <div className="xs:p-3 sm:p-4 md:p-6 h-1/2 md:h-1/3 lg:p-3 lg:flex lg:flex-col lg:justify-start lg:gap-2 lg:basis-8/12">
+              <div className="flex items-center-safe font-bold gap-1.5 lg:gap-3 text-lg">
                 <div className="bg-[#f2f2f0] p-1 rounded-md border border-[#E9EBE8]">
-                  <Image src={item.iconTitle} width={20} height={20} alt={item.title} />
+                  <Image src={item.iconTitle} width={isMobile == true ? 15 : 26} height={isMobile == true ? 15 : 26} alt={item.title} />
                 </div>
-                <h4 className="text-sm md:text-lg font-bold text-nik-foreground">{item.title}</h4>
+                <h4 className="text-sm sm:text-md md:text-lg font-bold text-nik-foreground">{item.title}</h4>
               </div>
-              <div className="font-normal mt-2 text-justify text-nik-foreground leading-6 text-xs md:text-md md:leading-8 lg:leading-8 lg:text-[14px]">
+              <div className="font-normal mt-2 text-justify text-nik-foreground leading-6 sm:leading-8 text-xs sm:text-sm md:text-[16px] md:leading-8 lg:leading-8 lg:text-[14px] lg:w-[421px]">
                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
                 ستون و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد
                 گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد.
               </div>
-              <div className="lg:mt-12">
+              <div className="lg:mt-10">
                 <Button
                   variant={"focused"}
                   className="rounded-lg group mt-3.5 flex items-center text-[12px] justify-between text-sm w-35.75 lg:w-43.25 h-10 lg:h-11"
@@ -60,15 +61,15 @@ const TabsComponent = () => {
               </div>
             </div>
             {/*//^ BOTTOM SECTION */}
-            <div id="TABS__VIDEO" className="relative basis-1/3 h-[215px] lg:basis-1/2 inset-0 flex items-center justify-center">
+            <div id="TABS__VIDEO" className="relative basis-1/3 h-[215px] md:h-1/2 lg:h-auto lg:basis-1/2 inset-0 flex items-center justify-center">
               <Sheet>
-                <SheetTrigger>
+                <SheetTrigger className="bg-transparent p-2 pb-0 lg:rounded-t-[24px] lg:p-3 lg:bg-nik-gray ">
                   <Image
                     src={isTablet == true && item.mobileThumbnail ? item.mobileThumbnail : item.thumbnail}
                     width={isXS ? 400 : 600}
-                    height={200}
+                    height={isXS ? 200 : 400}
                     alt="thumbnail"
-                    className="mt-5 xs:mt-2 sm:-mt-5 lg:mt-10"
+                    className="mt-4 xs:mt-2 lg:mt-5 rounded-t-[17px] h-full"
                   />
                 </SheetTrigger>
                 <SheetContent showCloseButton={false} dir="rtl" side="bottom" className="border-t-6 h-84 border-t-nik-primary rounded-t-xl">
